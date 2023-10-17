615db9f67ab4909fca7938e42f91ea8d2c3323cb
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-10-17 13:15:48 +0200

    Fix spelling, me stupid

--- Serie_4/Serie4.hs
+++ Serie_4/Serie4.hs
@@ -1,6 +1,6 @@
 module Serie4 where
     listDoubles :: String -> [String]
-    listDoubles scentence = let list = words scentence in [list!!index | index <- [0..(length list - 2)], list!!index == list!!(index+1)]
+    listDoubles sentence = let list = words sentence in [list!!index | index <- [0..(length list - 2)], list!!index == list!!(index+1)]
 
     testDoubles :: String -> Bool
-    testDoubles scentence = not (null (listDoubles scentence))
+    testDoubles sentence = not (null (listDoubles sentence))
