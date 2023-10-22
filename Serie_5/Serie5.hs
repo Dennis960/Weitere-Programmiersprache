614defeef5d5d94b8c69f1410bd694ceb9b035fe
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-10-22 15:17:38 +0200

    Less code = better

--- Serie_5/Serie5.hs
+++ Serie_5/Serie5.hs
@@ -3,21 +3,19 @@ module Serie5 where
     -- im Fall von strenger Monotonie müsste nur das "=" aus "<=" und ">=" in "isAscending" und "isDescending" entfernt werden
     monotoneFolge = [1,1,2,3,3,4,5,6,10,22]
     isMonotonous :: Ord a => [a] -> Bool
+    isMonotonous [] = True
     isMonotonous list = isAscending list || isDescending list where
-        isAscending [] = True
         isAscending [_] = True
         isAscending (x:xs) = x <= head xs && isAscending xs
-        isDescending [] = True
         isDescending [_] = True
         isDescending (x:xs) = x >= head xs && isDescending xs
 
     alternierendeFolge = [-1,5,-2,20,-11,13,-11,13]
     isAlternating :: (Ord a, Num a) => [a] -> Bool
+    isAlternating [] = True
     isAlternating list = isAlternatingPositive list || isAlternatingNegative list where
-        isAlternatingPositive [] = True
         isAlternatingPositive [x] = x >= 0
         isAlternatingPositive (x:xs) = x >= 0 && head xs <= 0 && isAlternatingNegative xs
-        isAlternatingNegative [] = True
         isAlternatingNegative [x] = x <= 0
         isAlternatingNegative (x:xs) = x <= 0 && head xs >= 0 && isAlternatingPositive xs
 
